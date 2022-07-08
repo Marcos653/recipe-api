fc25a3d9d6dbb6987df07a1aa7cf083f1054bf6d
Your Name <viniciustatuta@gmail.com>
2022-07-07 21:28:04 -0300

    docker compose wait for db

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,25 @@
+"""
+Django command to wait for the database to be available
+"""
+from django.core.management.base import BaseCommand
+
+import time
+from django.db.utils import OperationalError
+from psycopg2 import OperationalError as Psycopg20pError
+
+class Command(BaseCommand):
+    """Django command to wait for database"""
+
+    def handle(self, *args, **options):
+        """Entrypoint for command."""
+        self.stdout.write('waiting for database')
+        db_up = False
+        while db_up is False:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except(Psycopg20pError, OperationalError):
+                self.stdout.write('Database unavailable, waiting 1 second...')
+                time.sleep(1)
+
+            self.stdout.write(self.style.SUCCESS('Database available!'))
